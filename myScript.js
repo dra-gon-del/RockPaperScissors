6197f92cda1db0e0ca001b3eeef1a82efff3127e
A.Westwood <personal.email.aw1@gmail.com>
2023-10-11 15:05:34 +0100

    3. check if working, write plan, write array with options

---
+++ myScript.js
@@ -0,0 +1,9 @@
+console.log("hello world!")
+
+//1. create array with rps options.
+//2. create function with random computer choice with array.
+//3. write function that plays single round of rps, each round you return a result like "you win x beats y".
+//4. write new function called game which plays 5 rounds then announces winner at the end.
+
+//1. 
+let options = ['rock', 'paper', 'scissors'];
